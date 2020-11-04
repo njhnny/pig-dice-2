c46e5614dda86d1c8ecdfd4868f64ec37c79e49f
Nick Hennessy <njhnny@gmail.com>
2020-11-04 15:51:23 -0800

    create hold function

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
 // BUSINESS LOGIC
+
+
+
 let score = 0;
+let total = 0;
 function diceRoll() {
   let dice = Math.floor( Math.random() * 6 ) +1;
-  alert(dice);
+  
      if (dice > 1) {
        score = (score += dice);
        
+     } else {
+       score = (0);
      }
-     $("p").text(score);
+     $("p","#score").text(score);
+     $("p","#dice").text(dice);
  };
+ function hold() {
+  let holdDisplay = (total += score);
+  
+  $("p","#total").text(holdDisplay);
+  $("p","#dice").text("");
+  score = (0);
+ };
+ 
+ 
 $(document).ready(function() {
   $("#btn").click(diceRoll);
-  
+  $("#holdbtn").click(hold);
   
  });
  
